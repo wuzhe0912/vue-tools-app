6b9b26686bd6ef582e33f2c9d168ccb20b4c20a8
zhethon <kgb00128@gmail.com>
2018-05-15 08:34:55 +0800

    Webpack
    - knowledge comment

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -21,16 +21,23 @@ const createLintingRule = () => ({
 
 module.exports = {
   context: path.resolve(__dirname, '../'),
+  // 表示 Webpack 編譯 Js 的入口文件
   entry: {
+    // key: value
     app: './src/main.js'
   },
+  // 輸出配置
   output: {
+    // 輸出文件的路徑
     path: config.build.assetsRoot,
+    // 文件名稱 name 對應上方入口文件的key值 ==> app ==>打包生成app.js
     filename: '[name].js',
+    // 請求靜態資源的絕對路徑
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
       : config.dev.assetsPublicPath
   },
+  // 導入時的配置
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
@@ -38,27 +45,35 @@ module.exports = {
       '@': resolve('src'),
     }
   },
+  // 對某種類型文件，通過某個loader作出處理
   module: {
     rules: [
       ...(config.dev.useEslint ? [createLintingRule()] : []),
+      // .vue類型的文件
       {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
       },
+      // js類型的文件
       {
         test: /\.js$/,
         loader: 'babel-loader',
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
+      // 圖片文件
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
+          // 文件大小
           limit: 10000,
+          // 文件名生成
+          // static/img/文件名.hash串.文件後綴
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
+      // 多媒體處理
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: 'url-loader',
@@ -67,6 +82,7 @@ module.exports = {
           name: utils.assetsPath('media/[name].[hash:7].[ext]')
         }
       },
+      // 字體icon處理
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
