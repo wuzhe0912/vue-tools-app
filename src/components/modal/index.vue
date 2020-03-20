f17103acdcb43012cf228db2e085f2afe4a8b532
Pitt Wu <kgb00128@gmail.com>
2020-03-20 17:18:41 +0800

    封裝基礎的modal組件

---
+++ src/components/modal/index.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+  .modal(v-show="showModal")
+    //- 遮罩
+    .mask
+    .modal__dialog
+      .modal__header
+        span 標題
+        a(href="javascript:;")
+      .modal__body
+        //- 為了擴充性，採用 slot 插入內容
+        slot(name="body")
+      .modal__footer
+        button 確定
+        button 取消
+</template>
+
+<script>
+export default {
+  name: 'modal',
+  props: {
+    /*
+      為了靈活性，可以將 modal 設計成多種類別，透過傳參來選擇顯示對應的類型
+      例如大、中、小、表單
+    */
+    modalType: {
+      type: String,
+      default: 'form'
+    },
+    // 標題
+    title: String,
+    /*
+      button type：
+      1. sure & cancel
+      2. sure
+      3. cancel
+    */
+    btnType: String,
+    // button text
+    sureText: {
+      type: String,
+      default: '確定'
+    },
+    cancelText: {
+      type: String,
+      default: '取消'
+    },
+    showModal: Boolean
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import './style';
+</style>
--- src/views/todo/index.vue
+++ src/views/todo/index.vue
@@ -1,13 +1,29 @@
 <template lang="pug">
-  .home__wrap home
+  .home__wrap
+    //- Modal(
+    //-   :showModal="showModal"
+    //-   title="新增待辦事項",
+    //-   sureText="確定新增",
+    //-   btnType="2",
+    //-   modalType="middle",
+    //- )
+    //-   //- slot 新的寫法需加入 template
+    //-   template(v-slot:body)
+    //-     input.form__input(placeholder="請輸入代辦事項內容")
 </template>
 
 <script>
+import Modal from '@/components/modal'
+
 export default {
   name: 'todo',
-  components: {},
+  components: {
+    Modal
+  },
   data () {
-    return {}
+    return {
+      showModal: true
+    }
   },
 
   mounted () {
